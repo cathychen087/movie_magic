8bf39487f57df30d26dabde5ef35020651e17e36
Cathy <cathy@example.com>
2025-04-03 23:06:06 +0800

    Update .gitignore to track db.sqlite3 and staticfiles

--- movie_magic/settings.py
+++ movie_magic/settings.py
@@ -129,7 +129,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # WhiteNoise configuration
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
